5b976cc6d8ba96dd65e51f6bae02552ef42fc33d
Armin Köfler <arminko82@gmail.com>
2017-11-15 18:59:48 +0100

    Core code finished, never tested

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+"use strict";
+
 const http = require("http");
 const CronJob = require('cron').CronJob;
-const isTunnelClosed = require('scannor.js');
+const evaluateTunnel = require('./scannor.js');
 
 const PORT = 8081;
 
+// long running server
 http.createServer(function (request, response) {
 
     response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -11,13 +14,12 @@ http.createServer(function (request, response) {
     response.end('Hello World\n');
 }).listen(PORT);
 
-// Console will print the message
-console.log(`Server running at http://127.0.0.1:{PORT}/`);
-
-
 //  monday, turesday, wednesday and friday every second between 05:00 and 08:00
 new CronJob('00 * 5-8 * * 1-3,5', function() {
-    if(isTunnelClosed()) {
-            // TODO maybe https://developer-blog.net/raspberry-pi-sprachausgabe/
-    }
+    isTunnelClosed(reactOnClosedTunnel);
 }, null, true, 'Europe/Vienna');
+
+// bool parameter
+function reactOnClosedTunnel(tunnelPassable) {
+// TODO maybe https://developer-blog.net/raspberry-pi-sprachausgabe/
+}
--- scannor.js
+++ scannor.js
@@ -1,26 +1,40 @@
+"use strict";
+
+const https = require('https');
+const jsdom = require('jsdom');
+var $ = require('jQuery');
+
+const SOURCE = "https://www.oeamtc.at/verkehrsservice/";
 const TUNNEL = "Plabutsch";
 const indexText = 4;
 const indexSymbol = 1;
 
 String.prototype.contains = (text, term) => text.indexOf(term) !== -1;
 const announcementFilter = (n) => contains(n, TUNNEL);
+const checkTunnel (reactor) =>  fetchDataSource(evaluteTunnel, reactor);
+}
 
-function isTunnelClosed() {
-    var qra = $(".col" + indexText).toArray();
-    var result = qra.find(announcementFilter);
-    if(result) {
-        var symbol = result.previousSibling.previousSibling.previousSibling.innerHTML.toString();
-        var passable = symbol.contains('gesperrt');
-        if(!passable) {
-            $("#result").val("text", result);
-        } else {
-            console.log('nothing 2');
+function fetchDataSource(evaluator, reactor) {
+    jsdom.env({
+        url: SOURCE,
+        src: [jquery],
+        done: function (errors, window) {
+            reactor(evaluator());
+        });
+    });
+}
+
+    function evaluteTunnel() {
+        var qra = $(".col" + indexText).toArray();
+        var result = qra.find(announcementFilter);
+        if(result) {
+            var symbol = result.previousSibling.previousSibling.previousSibling.innerHTML.toString();
+            var passable = symbol.contains('gesperrt');
+            return passable;
         }
-    } else {
-        console.log('nothing');
+        return true; // wo know nothing => no reation
     }
-}
 
-if(typeof module !== 'undefined') {
-	module.exports = isTunnelClosed;
-}
+    if(typeof module !== 'undefined') {
+        module.exports = checkTunnel;
+    }
