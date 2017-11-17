b3604c56741293f961172c232e8bbe5b5f859c23
Köfler <kfa@jr1.local>
2017-11-17 12:31:26 +0100

    Added express http server to project

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 "use strict";
 
 const http = require("http");
+const app = require('express')();
 const CronJob = require('cron').CronJob;
 const evaluateTunnel = require('./scannor.js');
 
 const PORT = 8081;
 
 // long running server
-http.createServer(function (request, response) {
+http.createServer(app).listen(PORT);
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
-}).listen(PORT);
+app.get("/", function(req, res) {
+   send(res, 'index.html');
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+   // Send the response body as "Hello World"
+//   response.end('Hello World\n');
+
+});
 
 //  monday, turesday, wednesday and friday every second between 05:00 and 08:00
 new CronJob('00 * 5-8 * * 1-3,5', function() {
