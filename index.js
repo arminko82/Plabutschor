7df1b3cb4c4218b05cfd040f6b75bdfb735c89e4
Armin Köfler <arminko82@gmail.com>
2017-11-18 11:22:29 +0100

    Scanning has been tested and is working

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 "use strict";
 
+const PORT = 8081;
+
 const http = require("http");
 const app = require('express')();
 const CronJob = require('cron').CronJob;
-const evaluateTunnel = require('./scannor.js');
+const isTunnelClosed = require('./scannor.js');
 
-const PORT = 8081;
+function init() {
+    // long running server
+    http.createServer(app).listen(PORT);
 
-// long running server
-http.createServer(app).listen(PORT);
+    app.get("/", function(req, res) {
+       send(res, 'index.html');
+    //   response.writeHead(200, {'Content-Type': 'text/plain'});
+       // Send the response body as "Hello World"
+    //   response.end('Hello World\n');
 
-app.get("/", function(req, res) {
-   send(res, 'index.html');
-//   response.writeHead(200, {'Content-Type': 'text/plain'});
-   // Send the response body as "Hello World"
-//   response.end('Hello World\n');
+    });
 
-});
-
-//  monday, turesday, wednesday and friday every second between 05:00 and 08:00
-new CronJob('00 * 5-8 * * 1-3,5', function() {
-    isTunnelClosed(reactOnClosedTunnel);
-}, null, true, 'Europe/Vienna');
+    //  monday, turesday, wednesday and friday every second between 05:00 and 08:00
+    new CronJob('00 * 5-8 * * 1-3,5', function() {
+        isTunnelClosed(reactOnClosedTunnel);
+    }, null, true, 'Europe/Vienna');
+}
 
 // bool parameter
-function reactOnClosedTunnel(tunnelPassable) {
+function reactOnClosedTunnel(closed) {
+    console.log('Is Tunnel closed? ' + (closed ? 'yes' : 'no'));
 // TODO maybe https://developer-blog.net/raspberry-pi-sprachausgabe/
 }
+
+isTunnelClosed(reactOnClosedTunnel); // test
+//init();
--- scannor.js
+++ scannor.js
@@ -1,31 +1,38 @@
 "use strict";
 
 const https = require('https');
-const jsdom = require('jsdom');
-var $ = require('jQuery');
 
-const SOURCE = "https://www.oeamtc.at/verkehrsservice/";
+const SOURCE = "https://www.oeamtc.at/verkehrsservice/proxy.php?url=current/";
 const TUNNEL = "Plabutsch";
 const indexText = 4;
 const indexSymbol = 1;
 
-String.prototype.contains = (text, term) => text.indexOf(term) !== -1;
-const announcementFilter = (n) => contains(n, TUNNEL);
-const checkTunnel = (reactor) =>  fetchDataSource(evaluteTunnel, reactor);
-}
+var $ = {};
 
-function fetchDataSource(evaluator, reactor) {
-    jsdom.env({
-        url: SOURCE,
-        src: [jquery],
-        done: function (errors, window) {
-            reactor(evaluator());
-        });
+function fetch(done, reactor) {
+    https.get(SOURCE, (resp) => {
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        reactor(done(data));
+      });
+    }).on("error", (err) => {
+      reactor(done(null))
     });
 }
 
-function evaluteTunnel() {
-    var qra = $(".col" + indexText).toArray();
+function isTunnelClosed(reactor) {
+    fetch(isTunnelClosedDecider, reactor);
+}
+
+function isTunnelClosedDecider(data) {
+    //console.log(data);
+    var c = data.indexOf(TUNNEL) !== -1;
+    return c;
+    /*
+    var qra = $("td.col" + indexText).toArray();
     var result = qra.find(announcementFilter);
     if(result) {
         var symbol = result.previousSibling.previousSibling.previousSibling.innerHTML.toString();
@@ -33,8 +40,9 @@ function evaluteTunnel() {
         return passable;
     }
     return true; // wo know nothing => no reation
+    */
 }
 
 if(typeof module !== 'undefined') {
-    module.exports = checkTunnel;
+    module.exports = isTunnelClosed;
 }
