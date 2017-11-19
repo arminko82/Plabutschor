c1e6718c0614ed6b0eafc3fee0ddd71d8e3afe64
Armin Köfler <arminko82@gmail.com>
2017-11-19 10:24:24 +0100

    A currently running alarm can be stopped using the web frontend Main switches have been added to index.js which allow configuring the test settings more easily

--- index.js
+++ index.js
@@ -5,28 +5,48 @@ const CronJob = require('cron').CronJob;
 const exec = require('child_process').exec;
 const isRouteBlocked = require('./scannor.js');
 const Tools = require('./tools.js');
+const express = require('express');
 
-const ENABLE_CRON = false;
-const ENABLE_FRONTEND = false;
-const ENABLE_DIRECT_CALL = true;
+/*
+ * Main switsches
+ */
+const ENABLE_CRON = true;
+const ENABLE_FRONTEND = true;
+const ENABLE_DIRECT_CALL = false;
+const USE_TEST_INTERVAL = false;
 
+const FRONTEND_BASE_DIR = '.';
 const FRONTEND_PORT = 8081;
+
 //  monday, tuesday, wednesday and friday every minute between 05:00 and 08:00
 const CRON_INTERVALS = '00 * 5-8 * * 1-3,5';
+const TEST_INTERVALS = '00 * * * * *';
+
+const mApp = express();
+var mCurrentAlarmProcess = null;
 
 // Global init and config
 function init() {
     if(ENABLE_FRONTEND) {
-        http.createServer(app).listen(FRONTEND_PORT);
-        app.get("/", function(req, res) {
-            send(res, 'index.html');
-            //   response.writeHead(200, {'Content-Type': 'text/plain'});
-            // Send the response body as "Hello World"
-            //   response.end('Hello World\n');
+        http.createServer(mApp).listen(FRONTEND_PORT);
+        mApp.get("/", (req, res) => send(res, 'index.html'));
+        mApp.get("/deactive", (req, res) => {
+            if(mCurrentAlarmProcess !== null) {
+                mCurrentAlarmProcess.kill();
+                mCurrentAlarmProcess = null;
+            }
+            send(res, 'done.html');
         });
+
+        function send(res, item) {
+            res.sendFile(item, {root: FRONTEND_BASE_DIR});
+        }
+        Tools.log("Server runs at port " + FRONTEND_PORT);
     }
+
     if(ENABLE_CRON) {
-        new CronJob(CRON_INTERVALS, function() {
+        const interval = USE_TEST_INTERVAL ? TEST_INTERVALS : CRON_INTERVALS;
+        new CronJob(interval, function() {
             isRouteBlocked(reactOnBlockage);
         }, null, true, 'Europe/Vienna');
     }
@@ -35,12 +55,13 @@ function init() {
 // bool parameter
 function reactOnBlockage(reportText) {
     Tools.log('Reporting incident: ' + reportText);
-    exec(`espeak "${reportText}" -v german`, (err, stdout, stderr) => {
-        if (err) {
-            Tools.log('FAILED to report incident.');
-            return;
-        }
-    });
+    mCurrentAlarmProcess = exec(`espeak "${reportText}" -v german`,
+        (err, stdout, stderr) => {
+            if (err) {
+                Tools.log('FAILED to report incident.');
+                return;
+            }
+        });
 }
 
 if(ENABLE_DIRECT_CALL)
