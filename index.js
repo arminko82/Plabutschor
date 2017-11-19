9e07ac109d4ab0ee98d44dacf70da37c7a5d4a13
Armin Köfler <arminko82@gmail.com>
2017-11-19 09:31:26 +0100

    Startup is easily configured

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 "use strict";
 
-const PORT = 8081;
-
 const http = require("http");
-const app = require('express')();
 const CronJob = require('cron').CronJob;
-const { exec } = require('child_process');
+const exec = require('child_process');
 const isRouteBlocked = require('./scannor.js');
 const Tools = require('./Tools.js');
 
+const ENABLE_CRON = false;
+const ENABLE_FRONTEND = false;
+const ENABLE_DIRECT_CALL = true;
+
+const FRONTEND_PORT = 8081;
+//  monday, tuesday, wednesday and friday every minute between 05:00 and 08:00
+const CRON_INTERVALS = '00 * 5-8 * * 1-3,5';
+
 // Global init and config
 function init() {
-    http.createServer(app).listen(PORT);
-
-    app.get("/", function(req, res) {
-        send(res, 'index.html');
-        //   response.writeHead(200, {'Content-Type': 'text/plain'});
-        // Send the response body as "Hello World"
-        //   response.end('Hello World\n');
-    });
-
-    //  monday, turesday, wednesday and friday every second between 05:00 and 08:00
-    new CronJob('00 * 5-8 * * 1-3,5', function() {
-        isRouteBlocked(reactOnBlockage);
-    }, null, true, 'Europe/Vienna');
+    if(ENABLE_FRONTEND) {
+        http.createServer(app).listen(FRONTEND_PORT);
+        app.get("/", function(req, res) {
+            send(res, 'index.html');
+            //   response.writeHead(200, {'Content-Type': 'text/plain'});
+            // Send the response body as "Hello World"
+            //   response.end('Hello World\n');
+        });
+    }
+    if(ENABLE_CRON) {
+        new CronJob(CRON_INTERVALS, function() {
+            isRouteBlocked(reactOnBlockage);
+        }, null, true, 'Europe/Vienna');
+    }
 }
 
 // bool parameter
@@ -37,5 +43,6 @@ function reactOnBlockage(reportText) {
     });
 }
 
-isRouteBlocked(reactOnBlockage); // test
-//init();
+if(ENABLE_DIRECT_CALL)
+    isRouteBlocked(reactOnBlockage);
+init();
