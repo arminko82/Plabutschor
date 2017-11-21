da8e8fbe7206340bddf4752ed036a4609877c91a
Armin Köfler <arminko82@gmail.com>
2017-11-21 08:31:54 +0100

    Scannor now uses correct data source and filters by time BUT: The time filter is buggy at the moment

--- scannor.js
+++ scannor.js
@@ -1,17 +1,19 @@
 "use strict";
 
-const USE_TEST_INPUT = false;
+const USE_TEST_INPUT = true;
 const ARCHIVE = true;
 
 const https = require('https');
 const fs = require('fs');
+const moment = require('moment');
 const Tools = require('./tools.js');
 const archive = require('./archive.js');
 
-const SOURCE = "https://www.oeamtc.at/verkehrsservice/proxy.php?url=current/";
+const SOURCE = "https://www.oeamtc.at/verkehrsservice/proxy.php?url=current/?count=9999&include=cameras";
 const TUNNEL = "Plabutsch";
 const BLOCKAGE = 'Sperre';
 const DESC_ID = 'oeamtc.long-description';
+const TIME_ID = 'oeamtc.start-time';
 
 function accountPotentialBlockage(reactor) {
     fetchTraffic(decider, reactor);
@@ -19,7 +21,7 @@ function accountPotentialBlockage(reactor) {
     function decider(data) {
         if(data === null) {
             Tools.log("Could not fetch data.");
-            return; // end handling
+            return false; // end handling
         }
         let traffic = JSON.parse(data)
             ['contents']['oeamtc.traffic-searchresult']['oeamtc.list']['oeamtc.traffic'];
@@ -27,24 +29,30 @@ function accountPotentialBlockage(reactor) {
             let report = test(traffic);
             if(report !== null) {
                 reactor(report);
-                break; // done on first encounter
+                return true; // done on first encounter and signal handled
             }
         }
+        return false;
     }
 
     function isTunnelBlocked(traffic) {
         const ID = 'A9';
         var incident = traffic.find(
             n => n["oeamtc.routeId"] === ID && n["oeamtc.type"] === BLOCKAGE &&
-            n[DESC_ID].indexOf(TUNNEL) !== -1);
+            n[DESC_ID].indexOf(TUNNEL) !== -1 && isToday(n));
         return incident === undefined ? null : incident[DESC_ID];
     }
 
     function isExpresswayBlocked(traffic) {
         const ID = 'S35';
         var incident = traffic.find(
-            n => n["oeamtc.routeId"] === ID && n ["oeamtc.type"] === BLOCKAGE);
+            n => n["oeamtc.routeId"] === ID && n ["oeamtc.type"] === BLOCKAGE &&
+            isToday(n));
         return incident === undefined ? null : incident[DESC_ID];
+    }
+
+    function isToday (n) {
+        return moment(n[TIME_ID]).isSame(moment(), 'day');
     }
 }
 
@@ -56,14 +64,17 @@ function fetchTraffic(decider) {
             let data = '';
             resp.on('data', (chunk) => data += chunk);
             resp.on('end', () =>  {
-                decider(data);
-                if(ARCHIVE) {
+                var incidentFound = decider(data);
+                if(ARCHIVE && incidentFound) {
                     archive(data);
                 }
             });
         }).on('error', (err) => decider(null));
     }
 }
+
+var m = moment();
+console.log(m);
 
 if(typeof module !== 'undefined') {
     module.exports = accountPotentialBlockage;
