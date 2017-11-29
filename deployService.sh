d08df78e57d0577392fafe5dbcf0e873f4cd300c
Armin Köfler <arminko82@gmail.com>
2017-11-29 21:07:05 +0100

    Service installer makes npm package check/update optional

--- deployService.sh
+++ deployService.sh
@@ -2,8 +2,11 @@
 set -e
 # see: https://github.com/zapty/forever-service
 sudo su $user << BASH
-    npm install -g forever
-    npm install -g forever-service
+    if [ "$1" = "with-npm" ]
+    then
+        npm install -g forever
+        npm install -g forever-service
+    fi
     forever-service install Plabutschor --script index.js
 BASH
 echo 'Hint: Add your email addresses for receiving alerts to mail.list'
