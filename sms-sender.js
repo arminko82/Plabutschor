2e063b3b85c1c715a234602f4acfdf585883448b
Armin Köfler <arminko82@gmail.com>
2018-12-08 23:19:51 +0100

    Fixed embarrassingly missed syntak error.

--- sms-sender.js
+++ sms-sender.js
@@ -1,44 +1,44 @@
 "use strict";
 
-const Tools = require('./tools.js');
-const https = require('http');
+const Tools = require("./tools.js");
+const http = require("http");
 
 // Sends sms messages to a given list of recipients.
 class SmsSender {
-    constructor() {
-        this.IP = "192.168.1.9";
-        const ERROR_MSG = "No SMS numbers found.";
-        try {
-            const r = /((\+\d\d)\s?|0)\d{3}\s?\d+/g;
-            const validNumbers =
+	constructor() {
+		this.IP = "192.168.1.9";
+		const ERROR_MSG = "No SMS numbers found.";
+		try {
+			const r = /((\+\d\d)\s?|0)\d{3}\s?\d+/g;
+			const validNumbers =
                 Tools.readLines("data/smsNumbers.txt").filter(l => r.test(l));
-            this.mNumberLine = validNumbers.join();
-            if(!this.mNumbers)
-                Tools.log(ERROR_MSG);
-        }
-        catch(e) {
-            Tools.log(ERROR_MSG);
-        }
-    }
-    static toUrl (t, ns) { return `http://${this.IP}/broadcast?text=${text}&numbers=${ns}`; }
+			this.mNumberLine = validNumbers.join();
+			if(!this.mNumbers)
+				Tools.log(ERROR_MSG);
+		}
+		catch(e) {
+			Tools.log(ERROR_MSG);
+		}
+	}
+	static toUrl (t, ns) { return `http://${this.IP}/broadcast?text=${text}&numbers=${ns}`; }
 
-    static broadcast(smsText) {
-        if(!this.mNumbers) {
-            return false;
-        }
-        http.get(toUrl(smsText, this.mNumbers), resp => { FINISH THIS
-          let data = '';
-          resp.on('data', (chunk) => data += chunk);
-          resp.on('end', () => {
-            console.log(JSON.parse(data).explanation);
-          });
+	static broadcast(smsText) {
+		if(!this.mNumbers) {
+			return false;
+		}
+		http.get(this.toUrl(smsText, this.mNumbers), resp => { //FINISH THIS
+			let data = "";
+			resp.on("data", (chunk) => data += chunk);
+			resp.on("end", () => {
+				console.log(JSON.parse(data).explanation);
+			});
 
-        }).on("error", (err) => {
-          console.log("Error: " + err.message);
-        });
-    }
+		}).on("error", (err) => {
+			console.log("Error: " + err.message);
+		});
+	}
 }
 
-if(typeof module !== 'undefined') {
-    module.exports = SmsSender;
+if(typeof module !== "undefined") {
+	module.exports = SmsSender;
 }
